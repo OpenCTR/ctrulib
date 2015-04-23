35345c6e706f8d7cd79aa5045f1ac6f962e510df
Alex Shaw <alex.shaw.as@gmail.com>
2015-04-22 21:04:01 -0500

    Update OpenCTR-Toolchain.cmake and add libctr check to libctru-config.cmake.in

--- cmake/Modules/OpenCTR-Toolchain.cmake
+++ cmake/Modules/OpenCTR-Toolchain.cmake
@@ -2,8 +2,14 @@
 # OpenCTR-Toolchain
 # -----------------
 # 
-# CMake toolchain (``CMAKE_TOOLCHAIN_FILE``) for enabling OpenCTR 
-# support.
+# CMake toolchain for enabling OpenCTR support.
+# 
+# This file may be included into a CMake project by either of the 
+# following methods:
+# 
+#  1. ``-DCMAKE_TOOLCHAIN_FILE=/path/to/OpenCTR-Toolchain.cmake``
+#  2. ``include(/path/to/OpenCTR-Toolchain.cmake)``
+# 
 # 
 
 ######################################################################
@@ -23,14 +29,39 @@
 # along with libctr.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
 
-if(NOT DEFINED CTR_ROOT)
-	message(SEND_ERROR "CTR_ROOT is not set")
+######################################################################
+
+# Include guard
+if(__OPENCTR_TOOLCHAIN)
+	return()
+endif()
+set(__OPENCTR_TOOLCHAIN TRUE)
+
+######################################################################
+
+# Was this file included through CMAKE_TOOLCHAIN_FILE or 
+# include()
+if(CMAKE_TOOLCHAIN_FILE STREQUAL CMAKE_CURRENT_LIST_FILE)
+	set(TOOLCHAIN_MODE TRUE)
+else()
+	set(TOOLCHAIN_MODE FALSE)
 endif()
 
-function(setcache var val)
-	set(${var} ${val} CACHE INTERNAL "${var}")
+######################################################################
+
+# Export variables, depending on if this file was included by 
+# CMAKE_TOOLCHAIN_FILE or through INCLUDE().
+function(ctrexport var val)
+	if(TOOLCHAIN_MODE)
+		set(${var} ${val} CACHE INTERNAL "${var}" FORCE)
+	else()
+		set(${var} ${val} PARENT_SCOPE)
+	endif()
 endfunction()
 
+######################################################################
+
+# Find ``arm-none-eabi-gcc``
 find_program(CMAKE_C_COMPILER_EXE 
 	NAMES
 		arm-none-eabi-gcc
@@ -40,9 +71,16 @@ find_program(CMAKE_C_COMPILER_EXE
 		bin
 	DOC
 		"Find the arm-none-eabi-gcc compiler"
-	NO_DEFAULT_PATH
 )
 
+# Error if arm-none-eabi-gcc was not found
+if(NOT CMAKE_C_COMPILER_EXE)
+	message(FATAL_ERROR "OpenCTR-Toolchain: arm-none-eabi-gcc not found")
+endif()
+
+######################################################################
+
+# Find arm-none-eabi-g++
 find_program(CMAKE_CXX_COMPILER_EXE 
 	NAMES
 		arm-none-eabi-g++
@@ -54,47 +92,67 @@ find_program(CMAKE_CXX_COMPILER_EXE
 		"Find the arm-none-eabi-g++ compiler"
 	NO_DEFAULT_PATH
 )
-if(CTR_ROOT)
-	setcache(CMAKE_SYSTEM_NAME "Generic")
-	setcache(CMAKE_SYSTEM_VERSION 1)
-	setcache(CMAKE_SYSTEM_PROCESSOR "ARM")
 
-	setcache(CMAKE_FIND_ROOT_PATH "${CTR_ROOT}")
-	setcache(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-	setcache(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-	setcache(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-	setcache(UNIX TRUE)
-	setcache(APPLE FALSE)
-	setcache(WIN32 FALSE)
-	setcache(CYGWIN FALSE)
-	setcache(MSVC FALSE)
-
-	setcache(CMAKE_CROSSCOMPILING TRUE)
-	setcache(CMAKE_EXECUTABLE_SUFFIX ".elf")
-	setcache(CMAKE_SKIP_RPATH TRUE)
-	setcache(BUILD_SHARED_LIBS FALSE)
-	setcache(CMAKE_STATIC_LIBRARY_PREFIX "lib")
-	setcache(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
-	setcache(CMAKE_SIZEOF_VOID_P "4")
-	setcache(CMAKE_AR "arm-none-eabi-ar")
-	setcache(CMAKE_RANLIB "arm-none-eabi-ranlib")
-	setcache(CMAKE_STANDARD_LIBRARIES "-lc -lm")
+# Error if arm-none-eabi-g++ was not found
+if(NOT CMAKE_CXX_COMPILER_EXE)
+	message(FATAL_ERROR "OpenCTR-Toolchain: arm-none-eabi-g++ not found")
 endif()
 
-if(CMAKE_C_COMPILER_EXE)
-	setcache(CMAKE_C_COMPILER "${CMAKE_C_COMPILER_EXE}")
-	setcache(CMAKE_C_COMPILER_ID "GNU")
-	setcache(CMAKE_C_COMPILER_ID_RUN TRUE)
-	setcache(CMAKE_C_COMPILER_FORCED TRUE)
-	setcache(CMAKE_COMPILER_IS_GNUCC TRUE)
-endif()
+######################################################################
 
-if(CMAKE_CXX_COMPILER_EXE)
-	setcache(CMAKE_CXX_COMPILER "${CMAKE_CXX_COMPILER_EXE}")
-	setcache(CMAKE_CXX_COMPILER_ID "GNU")
-	setcache(CMAKE_CXX_COMPILER_ID_RUN TRUE)
-	setcache(CMAKE_CXX_COMPILER_FORCED TRUE)
-	setcache(CMAKE_COMPILER_IS_GNUCXX TRUE)
-endif()
+# CMake System Information
+ctrexport(CMAKE_SYSTEM_NAME "Generic")
+ctrexport(CMAKE_SYSTEM_VERSION 1)
+ctrexport(CMAKE_SYSTEM_PROCESSOR "ARM")
 
+# CMake find_xxx() paths
+ctrexport(CMAKE_FIND_ROOT_PATH "${CTR_ROOT}")
+ctrexport(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+ctrexport(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+ctrexport(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# CMAKE_C_COMPILER
+ctrexport(CMAKE_C_COMPILER "${CMAKE_C_COMPILER_EXE}")
+ctrexport(CMAKE_C_COMPILER_ID "GNU")
+ctrexport(CMAKE_C_COMPILER_ID_RUN TRUE)
+ctrexport(CMAKE_C_COMPILER_FORCED TRUE)
+ctrexport(CMAKE_C_OUTPUT_EXTENSION ".o")
+ctrexport(CMAKE_COMPILER_IS_GNUCC TRUE)
+
+# CMAKE_CXX_COMPILER
+ctrexport(CMAKE_CXX_COMPILER "${CMAKE_CXX_COMPILER_EXE}")
+ctrexport(CMAKE_CXX_COMPILER_ID "GNU")
+ctrexport(CMAKE_CXX_COMPILER_ID_RUN TRUE)
+ctrexport(CMAKE_CXX_COMPILER_FORCED TRUE)
+ctrexport(CMAKE_CXX_OUTPUT_EXTENSION ".o")
+ctrexport(CMAKE_COMPILER_IS_GNUCXX TRUE)
+
+# Set platform properties
+ctrexport(UNIX TRUE)
+ctrexport(APPLE FALSE)
+ctrexport(WIN32 FALSE)
+ctrexport(CYGWIN FALSE)
+ctrexport(MSVC FALSE)
+
+# Enable C/C++ Support
+enable_language(C)
+enable_language(CXX)
+
+# Various CMake properties
+ctrexport(CMAKE_CROSSCOMPILING TRUE)
+ctrexport(CMAKE_EXECUTABLE_SUFFIX ".elf")
+ctrexport(CMAKE_SKIP_RPATH TRUE)
+ctrexport(BUILD_SHARED_LIBS FALSE)
+ctrexport(CMAKE_STATIC_LIBRARY_PREFIX "lib")
+ctrexport(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
+ctrexport(CMAKE_SIZEOF_VOID_P "4")
+ctrexport(CMAKE_STANDARD_LIBRARIES "-lc -lm")
+
+# Get directory that ``arm-none-eabi-gcc`` is installed in.
+get_filename_component(BIN_DIR "${CMAKE_C_COMPILER}" DIRECTORY)
+
+# Set ``CMAKE_AR`` and ``CMAKE_RANLIB``
+ctrexport(CMAKE_AR "${BIN_DIR}/arm-none-eabi-ar")
+ctrexport(CMAKE_RANLIB "${BIN_DIR}/arm-none-eabi-ranlib")
+
+######################################################################
